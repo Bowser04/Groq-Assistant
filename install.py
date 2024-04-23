e10f1db30e34d6e70dd70d71824ccebd1072603d
Bowser04 <82542192+Bowser04@users.noreply.github.com>
2024-04-23 16:39:55 +0200

    working shortcut

--- install.py
+++ install.py
@@ -1,13 +1,15 @@
 
 #from pyshortcuts import make_shortcut
-
+import winshell
 #make_shortcut('assistant.py', name='Windows Groq Assistant', icon='icon.ico')
 import os
+import shutil
 from win32com.client import Dispatch
 import pathlib
 
 path = os.path.join(str(pathlib.Path().resolve()), "Windows Groq Assistant.lnk")
-target = str(pathlib.Path().resolve())+r"\venv\Scripts\python.exe assistant.py"
+target = str(pathlib.Path().resolve())+'\\venv\\Scripts\\pythonw.exe'
+argument = "assistant.py"
 wDir = str(pathlib.Path().resolve())
 icon = str(pathlib.Path().resolve())+r"\icon.ico"
 
@@ -16,4 +18,9 @@ shortcut = shell.CreateShortCut(path)
 shortcut.Targetpath = target
 shortcut.WorkingDirectory = wDir
 shortcut.IconLocation = icon
+shortcut.arguments = argument
 shortcut.save()
+
+if input("add tou startup ? ([y]/n): ") == "y":
+    startup = winshell.startup()
+    shutil.copy("Windows Groq Assistant.lnk", startup)
