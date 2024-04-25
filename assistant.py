b98731fc7b37376890d5bca94b8956eccf0c932b
Bowser04 <82542192+Bowser04@users.noreply.github.com>
2024-04-25 11:53:05 +0200

    Update assistant.py
    fix bug:
    closing the settings takes us back to the main interface
    
    move password interface in first
    
    add simple gui to add prompt

--- assistant.py
+++ assistant.py
@@ -8,6 +8,7 @@ from infi.systray import SysTrayIcon
 import sys
 import signal
 client = None
+add_prompt_temp = []
 languages = [
     "Afrikaans",
     "Albanian",
@@ -176,6 +177,138 @@ def generate_event():
     global settings,decripted_api_key
     app.destroy()
     app = None
+    print("Generate")
+    print(settings["model"])
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": f"{action[actual_choice]}{pyperclip.paste()}",
+        }
+    ],
+    model=settings["model"],
+    )
+    if settings["clipboard"] == "on":
+        pyperclip.copy(chat_completion.choices[0].message.content)
+    if settings["write"] == "on":
+        time.sleep(0.5)
+        keyboard.press_and_release("down")
+        keyboard.press_and_release("enter")
+        keyboard.write(chat_completion.choices[0].message.content)
+    else:
+        customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+        customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+        app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+        app.geometry("800x440")
+        app.resizable(False,False)
+        app.title("Groq Assistant")
+        print(list(action.keys()))
+        textbox = customtkinter.CTkTextbox(master=app, width=800,height=440, corner_radius=0)
+        textbox.grid(row=0, column=0, sticky="nsew")
+        textbox.insert("0.0", chat_completion.choices[0].message.content)
+        textbox.configure(state="disabled")
+        app.mainloop()
+        app.destroy()
+        wait_key()
+        start_ui()
+def add_prompt_event():
+    global app
+    global actual_choice
+    global action
+    global languages
+    global settings, settings_checkbox
+    app.destroy()
+    app = None
+    customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+    customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+    app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+    app.geometry("400x400")
+    app.resizable(False,False)
+    app.title("Groq Assistant")
+    ###Seting var
+    label = customtkinter.CTkLabel(app,text="Prompt")
+    label.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+    write = customtkinter.CTkTextbox(app,width=400,height=100)
+    write.place(relx=0, rely=0.3, anchor=customtkinter.W)
+    label2 = customtkinter.CTkLabel(app,text="Name")
+    label2.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+    write2 = customtkinter.CTkTextbox(app,width=400,height=20)
+    write2.place(relx=0, rely=0.55, anchor=customtkinter.W)
+    ADD = customtkinter.CTkButton(app, text="Add", command=add_prompt,width=400,height=40)
+    ADD.place(relx=0.5, rely=0.7, anchor=customtkinter.CENTER)
+    global add_prompt_temp
+    add_prompt_temp = [write,write2]
+    app.mainloop()
+def add_prompt():
+    global add_prompt_temp,action,actual_choice,app
+    name = add_prompt_temp[1].get("0.0","end")[0:-1].replace("\n"," ").replace("::",": :")
+    prompt = add_prompt_temp[0].get("0.0","end")[0:-1].replace("\n"," ").replace("::",": :")
+    print(f"adding prompt '{name}' '{prompt}'")
+    tmp = open("action","a")
+    tmp.write(f"\n{name}::{prompt}")
+    tmp.close()
+    action_txt=open("action").read()
+    for i in range(len(action_txt.split("\n"))):
+        if action_txt.split("\n")[i] != "":
+            temp = action_txt.split("\n")[i]
+            temp_lst = temp.split("::")
+            action[temp_lst[0]] = temp_lst[1].replace("{language}",language)+" : \n"
+    print(action)
+    actual_choice = list(action.keys())[0]
+    app.destroy()
+
+def settings_event():
+    global app
+    global actual_choice
+    global action
+    global languages
+    global settings, settings_checkbox
+    app.destroy()
+    app = None
+    customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+    customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+    app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+    app.geometry("800x440")
+    app.resizable(False,False)
+    app.title("Groq Assistant")
+    ###Seting var
+    settings_checkbox["write"] = customtkinter.StringVar(value=settings["write"])
+    write = customtkinter.CTkCheckBox(app, text="automatic write response",
+                                     variable=settings_checkbox["write"], onvalue="on", offvalue="off")
+    write.place(relx=0.05, rely=0.1, anchor=customtkinter.W)
+    settings_checkbox["clipboard"] = customtkinter.StringVar(value=settings["clipboard"])
+    clipboard = customtkinter.CTkCheckBox(app, text="copy response to the clipboard",
+                                     variable=settings_checkbox["clipboard"], onvalue="on", offvalue="off")
+    clipboard.place(relx=0.05, rely=0.18, anchor=customtkinter.W)
+    optionmenu_lang = customtkinter.CTkOptionMenu(app, values=languages)
+    optionmenu_lang.set(settings["language"])
+    optionmenu_lang.place(relx=0.05, rely=0.26, anchor=customtkinter.W)
+
+    optionmenu_model = customtkinter.CTkOptionMenu(app, values=models)
+    optionmenu_model.set(settings["model"])
+    optionmenu_model.place(relx=0.05, rely=0.34, anchor=customtkinter.W)
+    add_prompt = customtkinter.CTkButton(app, text="Add prompt", command=add_prompt_event,width=28,height=8)
+    add_prompt.place(relx=0.9, rely=0.08, anchor=customtkinter.W)
+
+    app.mainloop()
+    settings["language"] = optionmenu_lang.get()
+    settings["model"] = optionmenu_model.get()
+    save_settings(settings_checkbox)
+    start_ui()
+
+import keyboard
+import time
+import sys
+
+def wait_key():
+    keyboard.wait("ctrl+alt+g")
+    time.sleep(0.5)
+    keyboard.press_and_release('ctrl+c')
+
+def start_ui():
+    global actual_choice
+    global action
+    global app
     if not decripted_api_key:
         def continue_password():
             global api_key_encripted
@@ -214,92 +347,6 @@ def generate_event():
         button = customtkinter.CTkButton(app,150,20,text="continue",command=continue_password)
         button.place(relx=0, rely=0.9, anchor=customtkinter.W)
         app.mainloop()
-        app.destroy()
-    print("Generate")
-    print(settings["model"])
-    chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": f"{action[actual_choice]}{pyperclip.paste()}",
-        }
-    ],
-    model=settings["model"],
-    )
-    if settings["clipboard"] == "on":
-        pyperclip.copy(chat_completion.choices[0].message.content)
-    if settings["write"] == "on":
-        time.sleep(0.5)
-        keyboard.press_and_release("down")
-        keyboard.press_and_release("enter")
-        keyboard.write(chat_completion.choices[0].message.content)
-    else:
-        customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
-        customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
-        app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-        app.geometry("800x440")
-        app.resizable(False,False)
-        app.title("Groq Assistant")
-        print(list(action.keys()))
-        textbox = customtkinter.CTkTextbox(master=app, width=800,height=440, corner_radius=0)
-        textbox.grid(row=0, column=0, sticky="nsew")
-        textbox.insert("0.0", chat_completion.choices[0].message.content)
-        textbox.configure(state="disabled")
-        app.mainloop()
-        app.destroy()
-        wait_key()
-        start_ui()
-def settings_event():
-    global app
-    global actual_choice
-    global action
-    global languages
-    global settings, settings_checkbox
-    print("Generate")
-    app.destroy()
-    app = None
-    customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
-    customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
-    app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-    app.geometry("800x440")
-    app.resizable(False,False)
-    app.title("Groq Assistant")
-    ###Seting var
-    settings_checkbox["write"] = customtkinter.StringVar(value=settings["write"])
-    write = customtkinter.CTkCheckBox(app, text="automatic write response",
-                                     variable=settings_checkbox["write"], onvalue="on", offvalue="off")
-    write.place(relx=0.05, rely=0.1, anchor=customtkinter.W)
-    settings_checkbox["clipboard"] = customtkinter.StringVar(value=settings["clipboard"])
-    clipboard = customtkinter.CTkCheckBox(app, text="copy response to the clipboard",
-                                     variable=settings_checkbox["clipboard"], onvalue="on", offvalue="off")
-    clipboard.place(relx=0.05, rely=0.18, anchor=customtkinter.W)
-    optionmenu_lang = customtkinter.CTkOptionMenu(app, values=languages)
-    optionmenu_lang.set(settings["language"])
-    optionmenu_lang.place(relx=0.05, rely=0.26, anchor=customtkinter.W)
-
-    optionmenu_model = customtkinter.CTkOptionMenu(app, values=models)
-    optionmenu_model.set(settings["model"])
-    optionmenu_model.place(relx=0.05, rely=0.34, anchor=customtkinter.W)
-    app.mainloop()
-    settings["language"] = optionmenu_lang.get()
-    settings["model"] = optionmenu_model.get()
-    save_settings(settings_checkbox)
-    wait_key()
-    start_ui()
-
-import keyboard
-import time
-import sys
-
-def wait_key():
-    keyboard.wait("ctrl+alt+g")
-    time.sleep(0.5)
-    keyboard.press_and_release('ctrl+c')
-
-def start_ui():
-    global actual_choice
-    global action
-    global app
     actual_choice = list(action.keys())[0]
     customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
     customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
